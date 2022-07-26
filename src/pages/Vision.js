3477736d62ab13c95922ea5035b8675bc2a3c5e4
heyyankitt <bhandariankit2075@gmail.com>
2022-07-26 19:31:10 +0545

    Vision page completion

---
+++ src/pages/Vision.js
@@ -0,0 +1,12 @@
+import '../pages/Vision.css'
+import React from 'react'
+
+export const Vision = () => {
+  return (
+    <>
+
+
+    <img className='roadmap' src='https://www.arweave.net/KeuFJsROZKojYNZbh5UZwTivNmceuDZ0bk57hy7uxDE' alt='roadmap'/>
+    </>
+  )
+}
