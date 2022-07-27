8caf6823f00f0819819e6539d2ce018606a617b7
heyyankitt <bhandariankit2075@gmail.com>
2022-07-27 22:39:10 +0545

    Homepage design WIP

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
+import '../pages/Home.css';
 import React from 'react'
 
 export default function Home() {
   return (
   <>
-
+    <img className='roadmap' src='https://www.arweave.net/2JPuPwXjkLDm80C-kUaQrFH4LCcQzxIpNpGvM07fSjE' alt='roadmap'/>
   </>
   )
 }
