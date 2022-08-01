b0adb0d6cf0642c4d31019cc98ffcffea66c4aeb
heyyankitt <bhandariankit2075@gmail.com>
2022-08-01 21:14:08 +0545

    traits modal UI changes

--- src/pages/Modals/Nftmodal.jsx
+++ src/pages/Modals/Nftmodal.jsx
@@ -47,14 +47,14 @@ export const Nftmodal = ({handleData,ukey,data}) => {
     <div className="modalContainer1">
       <div className="titleCloseBtn1">
       <div className="title1">
-        <h2>S.N:{nftName}</h2>
+        <h2 className='snno'>S.N:{nftName}</h2>
       </div>
         <button
           onClick={() => {
             handleData("",false);
           }}
         >
-          <CgCloseO size={35}/>
+          <CgCloseO size={35} style={{color: '#032854'}} />
         </button>
       </div>
       <div className="body1">
@@ -67,14 +67,6 @@ export const Nftmodal = ({handleData,ukey,data}) => {
           Accessories:&nbsp;{accessories}
       </div>
       <div className="footer1">
-        <button
-          onClick={() => {
-            handleData("",false);
-          }}
-          id="cancelBtn"
-        >
-          Ok
-        </button>
         <button
           onClick={() => {
             handleData("",false);
